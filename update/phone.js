81c39e1715cf8b97b4f3328d14462399faaa6945
alko3054 <alko3054@colorado.edu>
2019-02-13 15:38:23 -0700

    Add files via upload

---
+++ update/phone.js
@@ -0,0 +1,47 @@
+$(document).ready(function() { 
+ $("#numtab").show(); 
+ $("#contacttab").show();
+ $("#addtab").show();
+ $(".dialer").show();
+ $("#content_contactlist").hide(); 
+ $("#content_addcontact").hide(); 
+ $("#content_help").hide(); 
+ });
+
+$("#numtab").click(function() {
+	$("#content_contactlist").hide();
+	$("#content_addcontact").hide();
+	$("#content_help").hide(); 
+	$("#content_dialer").show();
+ });
+ 
+$("#contacttab").click(function() { 
+	$("#content_contactlist").show(); 
+	$("#content_dialer").hide();
+	$("#content_help").hide(); 
+	$("#content_addcontact").hide();
+});
+
+$("#addtab").click(function() { 
+	$("#content_addcontact").show();
+	$("#content_dialer").hide();
+	$("#content_help").hide(); 
+	$("#content_contactlist").hide();
+});
+
+$("#helptab").click(function() { 
+	$("#content_addcontact").hide();
+	$("#content_dialer").hide();
+	$("#content_help").show(); 
+	$("#content_contactlist").hide();
+});
+
+
+$(".dialer").click(function() {
+	document.getElementById("numfield").value = document.getElementById("numfield").value + document.getElementById(this.id).value;
+});
+
+$("#clear").click(function() {
+	document.getElementById("numfield").value = document.getElementById(this.id).value;
+	
+});
